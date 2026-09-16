afa26e11481d89b41c62c948cecd56a9a2209132
Vedant Subodh Gawande <vedantgawande89@gmail.com>
2026-09-16 14:25:43 +0530

    Add getter and setter for 'name' in RWOnly class

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
 }
