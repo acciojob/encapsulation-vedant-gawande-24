8023fee1faf95ed9a56c66b3adacdda608281a1c
Vedant Subodh Gawande <vedantgawande89@gmail.com>
2026-09-16 14:26:54 +0530

    Refactor getter to use 'this' keyword

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,16 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-
     private String name;
-
-    // Setter
+    
     public void setName(String name) {
         this.name = name;
     }
-
-    // Getter
+    
     public String getName() {
-        return name;
+        return this.name;
     }
 }
