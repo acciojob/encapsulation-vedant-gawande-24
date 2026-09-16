1d4445bf7a3fd7be57f73ea3776b2d51d331248f
Vedant Subodh Gawande <vedantgawande89@gmail.com>
2026-09-16 14:25:20 +0530

    Add main method to demonstrate RWOnly class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+
+        // Set value using setter
+        obj.setName("Vedant");
+
+        // Get value using getter
+        System.out.println(obj.getName());
+    }
 }
